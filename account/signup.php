6b38c1498aa30da94aa7da7222a8c9a4e86a821b
Cziperlan <czipri.gergo.bertalan@students.jedlik.eu>
2024-02-11 13:35:16 +0100

    Push 4
    Login és signup hozzáadása + rájöttem hogyan lehet css-t linkelni php-ba

---
+++ account/signup.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TechDepo - Regisztráció</title>
+    <style><?php include 'css/bootstrap.css'; ?></style>
+    <link rel="stylesheet" href="" type="text/css">
+</head>
+<body>
+    
+</body>
+</html>
