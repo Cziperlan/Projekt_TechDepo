7d27bf405f19a1e3ebd016bd7370d694becca11f
Cziperlan <czipri.gergo.bertalan@students.jedlik.eu>
2024-02-18 20:52:04 +0100

    Merülés előtti push

---
+++ js/slideshow.js
@@ -0,0 +1,28 @@
+let S = 1;
+let prev;
+showSlide(S);
+
+function Skip(n) {
+    showSlide(S += n);
+}
+
+function NowShow(n) {
+    showSlide(S = n);
+}
+
+function showSlide(n) {
+    let i;
+    let slidepics = document.getElementsByClassName("S");
+    let dots = document.getElementsByClassName("dind");
+    if (n > slidepics.length) { S = 1 }
+    if (n < 1) { S = slidepics.length }
+    for (i = 0; i < slidepics.length; i++) {
+        slidepics[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slidepics[S - 1].style.display = "block";
+    dots[S - 1].className += " active";
+
+}
