14e8bdd64db0f2b315299f20c70ab516a181ac0d
Cziperlan <czipri.gergo.bertalan@students.jedlik.eu>
2024-02-26 09:36:02 +0100

    Rólunk dropdown

--- pages/forum.php
+++ pages/forum.php
@@ -103,13 +103,13 @@ if(isset($_POST["submit"])){
                 <a href="">Brandek</a>
                 <a href="">Akciós Termékeink</a>
                 <a class="topright" href="">Rólunk</a>
-            <!--
+            
             <div class="sandwitch" onclick="tranSandwitch(this)">
                 <div class="bar1"></div>
                 <div class="bar2"></div>
                 <div class="bar3"></div>
             </div>
-        -->
+      
         </div>
     </header>
 
