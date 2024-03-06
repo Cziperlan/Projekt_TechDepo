3cc18ee5db2b9880bd78fec4233cd2e7bdcb2c17
Cziperlan <czipri.gergo.bertalan@students.jedlik.eu>
2024-03-06 23:25:56 +0100

    Reszponzív topnav
    in progress

--- js/sandwitch.js
+++ js/sandwitch.js
@@ -1,3 +1,24 @@
 function tranSandwitch(sand) {
     sand.classList.toggle("change");
   }
+
+
+  
+function openNav() {
+    document.getElementById("navSide").style.width = "250px";
+    document.getElementById("body").style.marginRight = "250px";
+  }
+  
+function closeNav() {
+    document.getElementById("navSide").style.width = "0";
+    document.getElementById("body").style.marginRight= "0";
+  }
+
+function dropSide() {
+  document.getElementsByClassName("sidedrop")
+    if (sidecont.style.display === "block") {
+    sidecont.style.display = "none";
+  } else {
+    sidecont.style.display = "block";
+  }
+}
--- pages/index.php
+++ pages/index.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <title>TechDepo - Főoldal</title>
 </head>
-
 <body id="body" onload="NowShow(1)">
     <header>
         <div class="header">
@@ -31,21 +30,32 @@
                     <a class="header-dis2" href="../pages/tracking.php">TRACK YOU ORDER</a>
                 </div>
         </div>
+        <div class="sidenav" id="navSide">
+                            <a href="" class="closebtn" onclick="closeNav()"><i class="fa fa-xmark"></i></a>
+                            <a href="">Ajánlataink</a>
+                            <a class="sidedrop" onclick="dropSide()">Termékeink <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                                <div class="sidecont">
+                                    <a href=""> - Számítógépek</a>
+                                    <a href=""> - Laptopok</a>
+                                </div>
+                            <a href="">Brandek</a>
+                            <a href="">Rólunk</a>
+                            <a href="">Fórum</a>
+                        </div>
         <div class="topnav">
-                <div class="menu">
-                   <a href=""><i class="fa fa-home" aria-hidden="true"></i></a>
-                   <a href=""><i class="fa fa-user" aria-hidden="true"></i></a>
-                   <a href=""><i class="fa fa-star" aria-hidden="true"></i></a>
-                </div>
-                <button class="sandwitch" onclick="tranSandwitch(this)">
-                    <div class="bar1"></div>
-                    <div class="bar2"></div>
-                    <div class="bar3"></div>
-                </button>
-                
+                    <div class="menu">
+                        <a href=""><i class="fa fa-home" aria-hidden="true"></i></a>
+                        <a href=""><i class="fa fa-user" aria-hidden="true"></i></a>
+                        <a href=""><i class="fa fa-star" aria-hidden="true"></i></a>
+                    </div>
+                    <button class="sandwitch dropbtn"  onclick="openNav()">
+                        <div class="bar1"></div>
+                        <div class="bar2"></div>
+                        <div class="bar3"></div>
+                    </button>
                 <a class="topnav-dis" href="../pages/index.php">Kezdőlap</a>
                 <a class="topnav-dis" href="">Ajánlataink</a>
-                <div class="dropdown top topnav-dis">
+                <div class="dropdown topnav-dis">
                     <button class="dropbtn">Termékeink</button>
                         <div class="dropdown-content">
                             <div class="subdown">
