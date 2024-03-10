6a262f477787fce1439f5a2f213398910e299437
KeszegMate <91196175+KeszegMate@users.noreply.github.com>
2024-03-10 13:43:05 +0100

    forum
    új css külön a fórumnak így nem lesz gondja a headerel meg a footerrel

--- pages/comment.php
+++ pages/comment.php
@@ -1,3 +1,7 @@
+<head>
+    <link rel="stylesheet" href="../css/forum.css">
+</head>
+
 <div class="comment">
   <h4>Név: <?php echo $data['name']; ?></h4>
   
@@ -16,63 +20,5 @@
     }
   }
   ?>
-  <style>
-      p.dátum{
-        font-style: italic;
-        font-size: small;
-        font-family: 'Courier New', Courier, monospace;
-        
-    }
-    .h4{
-        text-align: center;
-    }
-    .comment_box{
-        text-align: left;
-        font-size: large;
-        font-weight: bold;
-        font-family: Georgia, 'Times New Roman', Times, serif;
-    }
-   
-    
-    button {
-        border-radius: 1px;
-        background-color: #5ca1e1;
-        border: none;
-        color: #fff;
-        text-align: center;
-        font-size: 16px;
-
-        width: auto;
-        transition: all 0.5s;
-        cursor: pointer;
-
-        box-shadow: 0 10px 20px -8px rgba(0, 0, 0,.7);
-        }
-
-    button{
-        cursor: pointer;
-        display: inline-block;
-        position: relative;
-        transition: 0.5s;
-        }
-
-    button:after {
-        content: '»';
-        position: absolute;
-        opacity: 0;  
-        top: 14px;
-        right: -20px;
-        transition: 0.5s;
-        }
-
-    button:hover{
-        padding-right: 24px;
-        padding-left:8px;
-        }
-
-    button:hover:after {
-        opacity: 1;
-        right: 10px;
-        }
-  </style>
+  
 </div>
