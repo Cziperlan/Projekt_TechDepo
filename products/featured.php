062ed2f99e735c9658a2eefd85720fd44361854c
Cziperlan <czipri.gergo.bertalan@students.jedlik.eu>
2024-03-24 20:47:03 +0100

    Sidenav bugfix mégegyszer mert revertelve lett

--- products/featured.php
+++ products/featured.php
@@ -317,4 +317,5 @@
                 <p class="f-center">@ 2024-2024 www.techdepo.hu Minden jog fenntartva</p>
             </div>
     </footer>
+    <script src="../js/sandwitch.js"></script>
 </body>
